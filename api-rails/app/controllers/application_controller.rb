68778a050b6312cba77bf9770e48dc7c40f67cb4
Kenigbolo Meya Stephen <expensivestephen@hotmail.com>
2025-04-10 11:51:17 +0300

    🔒️ Add basic simple auth without persistence

--- api-rails/app/controllers/application_controller.rb
+++ api-rails/app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::API
+  before_action :set_current_user
+
+  private
+
+  def set_current_user
+    Current.user_id = request.headers['X-User-ID']
+    render json: { error: 'Missing X-User-ID header' }, status: :unauthorized if Current.user_id.blank?
+  end
 end
