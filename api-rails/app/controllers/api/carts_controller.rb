96a2fbac9d73e60b7aa5b32fb41ac8aec9133abb
Kenigbolo Meya Stephen <expensivestephen@hotmail.com>
2025-04-14 06:34:29 -0300

    🎨 clean up controllers and add test for carts controller

--- api-rails/app/controllers/api/carts_controller.rb
+++ api-rails/app/controllers/api/carts_controller.rb
@@ -14,14 +14,19 @@ module Api
         'cart_items.cart_item_options.part_option',
         'cart_items.cart_item_options.part_option.part'
       ]
+    rescue ActiveRecord::RecordInvalid => e
+      render json: { errors: e.record.errors.full_messages }, status: :unprocessable_entity
+    rescue ArgumentError => e
+      render json: { errors: [e.message] }, status: :unprocessable_entity
     end
 
     def create
       cart = Cart.create!(user_id: Current.user_id)
       render json: cart, status: :created
-    end
+    rescue ActiveRecord::RecordInvalid => e
+      render json: { errors: e.record.errors.full_messages }, status: :unprocessable_entity
+    rescue ArgumentError => e
+      render json: { errors: [e.message] }, status: :unprocessable_entity
+    end  
   end
 end
-
-
-
