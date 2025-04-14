38078c106a78ead2a26ca3912703d2355683c136
Kenigbolo Meya Stephen <expensivestephen@hotmail.com>
2025-04-14 06:04:03 -0300

    🎨 Update part options controller to handle error messages gracefully

--- api-rails/app/controllers/api/part_options_controller.rb
+++ api-rails/app/controllers/api/part_options_controller.rb
@@ -1,14 +1,51 @@
+# class Api::PartOptionsController < ApplicationController
+#   def create
+#     part = Part.find(params[:part_id])
+#     option = part.part_options.create!(part_option_params)
+#     render json: option, status: :created
+#   rescue ActiveRecord::RecordInvalid => e
+#     render json: { errors: e.record.errors.full_messages }, status: :unprocessable_entity
+#   end
+
+#   def update
+#     option = PartOption.find(params[:id])
+#     option.update!(part_option_params)
+#     render json: option
+#   rescue ActiveRecord::RecordInvalid => e
+#     render json: { errors: e.record.errors.full_messages }, status: :unprocessable_entity
+#   end
+
+#   def destroy
+#     option = PartOption.find(params[:id])
+#     option.destroy
+#     head :no_content
+#   end
+
+#   private
+
+#   def part_option_params
+#     params.require(:part_option).permit(:name, :base_price, :stock_status)
+#   end
+# end
 class Api::PartOptionsController < ApplicationController
   def create
     part = Part.find(params[:part_id])
     option = part.part_options.create!(part_option_params)
     render json: option, status: :created
+  rescue ActiveRecord::RecordInvalid => e
+    render json: { errors: e.record.errors.full_messages }, status: :unprocessable_entity
+  rescue ArgumentError => e
+    render json: { errors: [e.message] }, status: :unprocessable_entity
   end
 
   def update
     option = PartOption.find(params[:id])
     option.update!(part_option_params)
     render json: option
+  rescue ActiveRecord::RecordInvalid => e
+    render json: { errors: e.record.errors.full_messages }, status: :unprocessable_entity
+  rescue ArgumentError => e
+    render json: { errors: [e.message] }, status: :unprocessable_entity
   end
 
   def destroy
